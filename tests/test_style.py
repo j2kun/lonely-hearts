0ab5db384fbb0e24665fab5fad4091e5b0c42c65
Jeremy Kun <jeremy@21.co>
2016-10-22 10:45:28 -0700

    added pylint checker

--- tests/test_style.py
+++ tests/test_style.py
@@ -4,3 +4,8 @@ import subprocess
 def test_flake8():
     exit_status = subprocess.call(['flake8', '--exclude=venv/*,*test*'])
     assert exit_status == 0
+
+
+def test_pylint():
+    exit_status = subprocess.call(['pylint', '-E', 'hearts'])
+    assert exit_status == 0
