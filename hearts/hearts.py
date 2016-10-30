cb7d3d1837d0934f1dba0b788967b8803f19acff
Jeremy Kun <kun.jeremy@gmail.com>
2016-10-30 14:02:59 -0700

    hearts: implement basic Card class

--- hearts/hearts.py
+++ hearts/hearts.py
@@ -48,8 +48,35 @@ class Game(object):
 
 
 class Card(object):
-    def __init__(self):
-        pass
+    rank_values = {
+        '2': 2,
+        '3': 3,
+        '4': 4,
+        '5': 5,
+        '6': 6,
+        '7': 7,
+        '8': 8,
+        '9': 9,
+        'T': 10,
+        'J': 11,
+        'Q': 12,
+        'K': 13,
+        'A': 14,
+    }
+
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+
+    @staticmethod
+    def deserialize(serialized):
+        rank, suit = serialized[0], serialized[1]
+        card = Card(rank, suit)
+        card.validate()
+        return card
+
+    def serialize(self):
+        return ''.join([self.rank, self.suit])
 
 
 class Hand(list):
