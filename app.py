7a9c0c5fa0855d2518adcf1f855381c23575547a
Jeremy Kun <kun.jeremy@gmail.com>
2017-02-13 21:45:26 -0800

    api: add settings.py for proper configuration

--- app.py
+++ app.py
@@ -1,25 +1,15 @@
-import os
-
-import dotenv
 from flask import Flask
 from flask import render_template
 from flask_socketio import SocketIO
 from flask_socketio import emit
 from pymongo import MongoClient
 
-'''
-    Load the configuration variables from .env
-'''
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-dotenv_file = os.path.join(BASE_DIR, ".env")
-if os.path.isfile(dotenv_file):
-    dotenv.load_dotenv(dotenv_file)
+import settings
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'abc123')
+app.config['SECRET_KEY'] = settings.SECRET_KEY
 socketio = SocketIO(app)
-db_client = MongoClient(os.environ.get('DATABASE_URL'))
+db_client = MongoClient(settings.DATABASE_URL)
 
 
 @socketio.on('chat message', namespace='/chat')
@@ -50,4 +40,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host=settings.HOST, port=settings.PORT)
