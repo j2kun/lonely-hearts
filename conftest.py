60935445919ef1892ad51a3a0fb4e89b0c0597ac
Jeremy Kun <kun.jeremy@gmail.com>
2017-03-15 22:32:03 -0700

    test: make app.py use 'test' database instead of 'hearts'

--- conftest.py
+++ conftest.py
@@ -1,8 +1,8 @@
-from pymongo import MongoClient
 import pytest
 import socketio
 
 import settings
+settings.DATABASE_NAME = 'test'
 
 
 @pytest.fixture(scope="session")
@@ -20,6 +20,5 @@ def socket(request):
 
 @pytest.fixture(scope="module")
 def db(request):
-    test_db_name = 'test'
-    db_client = MongoClient(settings.DATABASE_URL)
-    return db_client[test_db_name]
+    from app import db_client
+    return db_client
