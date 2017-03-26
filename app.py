2187b253f44ead06b8cb6d4de464c2ff22be68f0
Jeremy Kun <kun.jeremy@gmail.com>
2017-03-25 17:11:17 -0700

    fix port issue

--- app.py
+++ app.py
@@ -73,4 +73,4 @@ def room(room_id):
 
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, host='0.0.0.0', port=5000)
--- static/socket_api.js
+++ static/socket_api.js
@@ -14,18 +14,19 @@ function RoomSocket(api_url) {
     this.api_url = api_url;
     this.socket = io(this.api_url);
     this.socket.on(CHAT, function(data) {
-        console.log(data);
+        console.log('User chatted: ' + data);
     });
 
     this.join_room = function(room_id) {
         this.room_id = room_id;
         this.socket.emit('join', {
-            room: this.room_id, 
+            room: this.room_id,
             username: 'Bill' + makeid(),
         });
     };
 
     this.chat = function(message) {
+        console.log('Submitting chat: ' + message);
         this.socket.emit(CHAT, message);
     }
 }
