f9937cf6ce1c5e61dd120d94b22d2c5ea77ae198
Daniel Nghiem <nghiem.daniel@yahoo.com>
2017-04-08 18:56:30 +0000

    api/tests: name some socket event tests

---
+++ hearts/api/tests/test_socket_events.py
@@ -0,0 +1,16 @@
+from tests.utils import json_data
+
+
+def test_db(api_client, db):
+    assert db.rooms.find({}).count() == 0
+
+
+def test_on_join_valid_room(api_client, socket_client, db):
+    db.rooms.insert_one({'room_id': '12345', 'users':[]})   # set up the room document
+    socket_client.emit('join', {'room': room_id, 'username': 'wat'})  #should it be socket_client or api_client?
+    pass
+
+
+def test_on_join_invald_room(api_client, db):
+    pass
+    
