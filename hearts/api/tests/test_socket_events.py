6bcd715a6196dee2ac4866a91f0564d70aa800b9
Daniel Nghiem <nghiem.daniel@yahoo.com>
2017-04-09 03:48:57 +0000

    api:  test rooms (in progress)

--- hearts/api/tests/test_socket_events.py
+++ hearts/api/tests/test_socket_events.py
@@ -6,11 +6,10 @@ def test_db(api_client, db):
 
 
 def test_on_join_valid_room(api_client, socket_client, db):
-    db.rooms.insert_one({'room_id': '12345', 'users':[]})   # set up the room document
-    socket_client.emit('join', {'room': room_id, 'username': 'wat'})  #should it be socket_client or api_client?
+    db.rooms.insert_one({'room_id': '12345', 'users': []})   # set up the room document
+    socket_client.emit('join', {'room': db.rooms['room_id'], 'username': 'wat'})  # should it be socket_client or api_client?
     pass
 
 
 def test_on_join_invald_room(api_client, db):
     pass
-    
