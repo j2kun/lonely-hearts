240fd45a1fc114f8feeaf12802b48288ff17e71e
Daniel Nghiem <nghiem.daniel@yahoo.com>
2017-04-30 00:11:10 +0000

    api/tests: use create_room() and get_room() in tests

--- hearts/api/tests/test_socket_events.py
+++ hearts/api/tests/test_socket_events.py
@@ -1,4 +1,5 @@
 from hearts.api.rooms import create_room
+from hearts.api.rooms import get_room
 
 
 def test_db(db):
@@ -8,22 +9,23 @@ def test_db(db):
 def test_create_room(db):
     test_room = create_room()
     assert test_room['users'] == []
+    test_room2 = create_room(('Daniel', 'Lauren', 'Erin', 'Jeremy'))
+    assert test_room2['users'] == ['Daniel', 'Lauren', 'Erin', 'Jeremy']
 
 
 def test_on_join_valid_room(api_client, socket_client, db):
-    db.rooms.insert_one({'users': [], 'testing': True})   # set up the room document for testing
-    test_room = db.rooms.find_one({'testing': True})
+    test_room = create_room()
     test_room_id = str(test_room['_id'])
 
     socket_client.emit('join', {'room': test_room_id, 'username': 'user_1'})
 
-    test_room = db.rooms.find_one({'testing': True})
+    test_room = get_room(test_room_id)
     assert test_room['users'] == ['user_1']
 
     socket_client.emit('join', {'room': test_room_id, 'username': 'user_2'})
     socket_client.emit('join', {'room': test_room_id, 'username': 'user_3'})
     socket_client.emit('join', {'room': test_room_id, 'username': 'user_4'})
-    test_room = db.rooms.find_one({'testing': True})
+    test_room = get_room(test_room_id)
     assert set(test_room['users']) == {'user_1', 'user_2', 'user_3', 'user_4'}
 
 
