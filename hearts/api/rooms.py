018f68cb1a32c2e46f0a471922277a584aec4e71
Jeremy Kun <kun.jeremy@gmail.com>
2017-04-30 14:04:31 -0700

    api: add some docstrings

--- hearts/api/rooms.py
+++ hearts/api/rooms.py
@@ -1,17 +1,46 @@
+'''
+Helper functions for interacting with rooms.
+
+A room object has the following fields
+
+    {
+      '_id': ObjectId,
+      'users': iterable of strings,
+    }
+'''
+
 from bson.objectid import ObjectId
 
 from hearts import mongo
 
 
 class RoomDoesNotExist(Exception):
+    '''
+    Exception thrown when trying to get a room that does not exist
+    '''
     pass
 
 
 class RoomCreateFailed(Exception):
+    '''
+    Exception thrown when room creation fails
+    '''
     pass
 
 
 def get_room(room_id):
+    '''
+    Get a room from the database
+
+    Input:
+        room_id: string or ObjectId
+
+    Output:
+        {
+          '_id': ObjectId,
+          'users': iterable of strings,
+        }
+    '''
     try:
         if not isinstance(room_id, ObjectId):
             room_id = ObjectId(room_id)
@@ -26,6 +55,18 @@ def get_room(room_id):
 
 
 def create_room(users=tuple()):
+    '''
+    Create a new room with the given users
+
+    Input:
+        users: iterable of strings
+
+    Output:
+        {
+          '_id': ObjectId,
+          'users': iterable of strings,
+        }
+    '''
     room_id = mongo.db.rooms.insert({'users': users})
     if room_id:
         return get_room(room_id)
