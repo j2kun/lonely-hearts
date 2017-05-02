0cbc906e8b61fe1dc538886200a613191004630f
Daniel Nghiem <nghiem.daniel@yahoo.com>
2017-05-02 02:42:47 +0000

    api: Add get_game() and create_game() in game.py.  Added test_create_game() in api/tests

--- hearts/api/socket_events.py
+++ hearts/api/socket_events.py
@@ -5,7 +5,6 @@ import flask_socketio as io
 from hearts import socketio
 from hearts import mongo
 from hearts.api.rooms import get_room
-from hearts.game.hearts import Game
 
 from bson.objectid import ObjectId
 
@@ -59,32 +58,3 @@ def on_leave(data):
     room = data['room']
     io.leave_room(room)
     chat(username + ' has left the room.', room=room)
-
-
-def create_game(room_id):
-    '''
-    Returns a string game_id.
-    If the room is not full, create_game will:
-        -Create a Game collection document in the db
-        -Store a game_id in the room
-        -Create and serialize a Game object from hearts.game.hearts.py
-         into the Game document.
-    '''
-    if is_room_full(room_id) is False:
-        room = get_room(room_id)
-        players = room['users']
-
-        new_game = Game(players, points_to_win=100)
-        game_data = new_game.serialize()
-
-        game_id = mongo.db.games.insert({
-            'room_id': str(room_id),
-            'users': players,
-            'data': game_data
-        })
-        mongo.db.rooms.update_one(
-            {'_id': ObjectId(room_id)},
-            {'$set': {'game_id': game_id}}
-        )
-    else:
-        pass  # Room is full
