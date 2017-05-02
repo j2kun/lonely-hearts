d5137c8983f25c75e91457b088f59801236b3f28
Jeremy Kun <kun.jeremy@gmail.com>
2017-05-01 21:19:13 -0700

    api/tests: fix tests expecting room_id

--- hearts/api/tests/test_rooms_api.py
+++ hearts/api/tests/test_rooms_api.py
@@ -1,16 +1,11 @@
-from bson.objectid import ObjectId
-
 from tests.utils import json_data
 
 
 def test_create_room(api_client, db):
     response = json_data(api_client.post('/rooms/'))
     assert response['url']
-    room_id = ObjectId(response['id'])
-    cursor = db.rooms.find({"_id": room_id})
-    assert cursor.count() == 1
-    assert cursor[0]['users'] == []
-
+    room_id = response['url'].split('/')[2]
+    assert room_id
     response = api_client.get(response['url'])
     assert response.status_code == 200
     assert response.content_type.startswith('text/html')
@@ -18,7 +13,7 @@ def test_create_room(api_client, db):
 
 def test_join_room(api_client, socket_client, db):
     response = json_data(api_client.post('/rooms/'))
-    room_id = response['id']
+    room_id = response['url'].split('/')[2]
     socket_client.emit('join', {'room': room_id, 'username': 'wat'})
 
     received = socket_client.get_received()
