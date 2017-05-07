5a22c9e9962b32762e539d6c77c0f0c6a989e6f7
Jeremy Kun <kun.jeremy@gmail.com>
2017-05-06 17:26:07 -0700

    api/socket_events: add 'sid' to the room document

--- hearts/api/socket_events.py
+++ hearts/api/socket_events.py
@@ -1,11 +1,12 @@
 from flask import session
+from flask import request
 import flask_socketio as io
 
 from hearts import socketio
 from hearts import mongo
 from hearts.api.rooms import get_room
 from hearts.api.games import create_game
-from hearts.api.games import GameCreateFailed
+from hearts.game.hearts import Player
 
 from bson.objectid import ObjectId
 
@@ -28,25 +29,30 @@ def on_join(data):
     username = data['username']
     room_id = data['room']
 
+    '''
+    The request context global is enhanced with a sid member that is set to a
+    unique session ID for the connection. This value is used as an initial room
+    where the client is added.
+    '''
+    socket_id = request.sid
+
     io.join_room(room_id)
     session['room'] = room_id
     mongo.db.rooms.update_one(
         {'_id': ObjectId(room_id)},
-        {'$push': {'users': username}}
-        )
+        {'$push': {'users': {'username': username, 'socket_id': socket_id}}}
+    )
     chat(username + ' has entered the room.', room=room_id)
 
     room = get_room(room_id)
     if len(room['users']) == 4:
-        try:
-            game, game_id = create_game(room_id, max_points=100)
-            session['game'] = str(game_id)
-            chat('The Hearts game has started.', room=room_id)
+        game, game_id = create_game(room_id, max_points=100)
+        session['game'] = str(game_id)
+        chat('The Hearts game has started.', room=room_id)
 
-            # Emit the serialized the game view for each player here.
-
-        except(GameCreateFailed):
-            pass
+        for user_info in room['users']:
+            serialized_for_player = game.serialize(for_player=Player(user_info['username']))
+            io.emit(serialized_for_player, room=user_info['socket_id'])
 
 
 @socketio.on('leave')
