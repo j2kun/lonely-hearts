904ee267b87eb540e348bffb2c0b6bb90469ed65
DanielNghiem <nghiemd@morainevalley.edu>
2017-08-11 14:12:58 -0700

    api: add message strings for passing and playing cards

--- hearts/api/strings.py
+++ hearts/api/strings.py
@@ -12,6 +12,9 @@ NOT_FOLLOWING_SUIT = 'you still have {} in your hand.'
 NOT_YOUR_TURN = "it's not your turn."
 NO_FIRST_TRICK_POINTS = 'you cannot play hearts or the queen of spades on the first trick.'
 
+PLAYED_A_CARD = '{} played {}.'
+PASSED_CARDS = 'You passed {} to {}.'
+RECEIVED_CARDS = '{} passed {} to you.'
 
 def message(error, error_reason):
     '''
@@ -19,3 +22,12 @@ def message(error, error_reason):
     with the string error_reason.
     '''
     return error.format({}, reason=error_reason)
+
+def play_card_message(player, card):
+    return PLAYED_A_CARD.format(player, card)
+
+def pass_cards_message(receiver, cards):
+    return PASSED_CARDS.format(cards, receiver)
+
+def receive_cards_message(passer, cards):
+    return RECEIVED_CARDS.format(passer, cards)
