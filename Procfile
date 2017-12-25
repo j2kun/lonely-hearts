d870c475691859a43056be5145f1a9992fb3e2a7
Jeremy Kun <j2kun@users.noreply.github.com>
2017-12-25 13:53:48 -0800

    add Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn run:app --worker-class gevent --log-file -
--- run.py
+++ run.py
@@ -2,9 +2,11 @@ from hearts import create_app
 from hearts import socketio
 
 app = create_app()
+'''
 socketio.run(
     app,
     host=app.config['HOST'],
     port=app.config['PORT'],
     debug=app.config['DEBUG']
 )
+'''
