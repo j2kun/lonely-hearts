76937aa3090d720a1984925cfaedac4c8e72ed9f
Daniel Nghiem <nghiem.daniel@yahoo.com>
2017-12-25 22:35:16 +0000

    configure log

--- run.py
+++ run.py
@@ -1,7 +1,17 @@
 from hearts import create_app
 from hearts import socketio
 
+import os
+import sys
+import logging
+from flask import Flask
+
 app = create_app()
+
+if 'DYNO' in os.environ:
+    app.logger.addHandler(logging.StreamHandler(sys.stdout))
+    app.logger.setLevel(logging.INFO)
+
 if app.config['DEBUG']:
     socketio.run(
         app,
